4207ca742d10f63b3afe4faeb4543eb17754f655
aguetler26 <aguetler26@icapital.at>
2018-04-23 14:20:37 +0200

    python datei erweitert / simple ein- und ausgabe

--- grundlagen-python.py
+++ grundlagen-python.py
@@ -1,2 +1,20 @@
 # grundlagen-python.py
 
+# Kommentare erfolgen mit hashtag
+
+# Ausgabe von Daten
+print("Hallo World")
+
+# Variable definieren (kann ohne Typ erfolgen)
+heimat = "Erde"
+
+# Mehrere Variable werden mit , getrennt
+print(heimat, "an World: ", "Hallo!")
+
+# Eingabe / liest Text(!) von der Konsole ein
+wer = input("Und wer bist du? ")
+
+# und gibt den Text wieder aus
+print ("Hallo", wer)
+
+
