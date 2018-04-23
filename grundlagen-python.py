e4d1e6e5b0d85a86c77ba0fea66ba98442f77ff9
aguetler26 <aguetler26@icapital.at>
2018-04-23 15:38:40 +0200

    gesamtsieger hinzugefuegt

--- grundlagen-python.py
+++ grundlagen-python.py
@@ -1,5 +1,5 @@
 # grundlagen-python.py
-
+import random
 # Kommentare erfolgen mit hashtag
 
 # Ausgabe von Daten
@@ -24,5 +24,30 @@ lieblingszahl = input("Was ist deine Lieblingszahl? ")
 print("Super, ich mag die Zahl", lieblingszahl)
 print("Aber die coolere Zahl", int(lieblingszahl)+10, "mag ich noch mehr!")
 
+runden = input("Wie viele Runden sollen wir spielen? ")
+runden = int(runden)
 
+siege_computer = 0
+siege_ich = 0
 
+for runde in range(1, runden+1):
+    sieger = ""
+    # zufallszahl erzeugen
+    zufallszahl = random.randint(1,6)
+    # ist zufallszahl 1,3 oder 5: bin ich der sieger
+    # sonst ist der computer der sieger
+    if (zufallszahl == 1 or zufallszahl == 3 or zufallszahl == 5):
+        sieger = "ich"
+        siege_ich += 1
+    else:
+        sieger = "computer"
+        siege_computer += 1
+    print("Runde", runde, "von", runden, ": Sieger:", sieger, ": gewuerfelt wurde:", zufallszahl)
+
+if (siege_ich > siege_computer):
+    print("Du gewinnst!")
+elif (siege_computer > siege_ich):
+    print("Du verlierst!")
+else:
+    print("Unentschieden")
+print("game over.")
